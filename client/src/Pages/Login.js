34ea7cac7a37e1316b46f32df7ffb8a218dcaffe
styxx3542 <madanpulkit67@gmail.com>
2023-03-18 21:16:27 +0530

    improved login page:

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -57,34 +57,46 @@ const Login = () => {
 
     return (
         <>
-            <div className="login text-center container mx-auto bg-[#F2EEDB] rounded-xl shadow border p-6 m-6">
-                <h1 className="text-3xl text-gray-700 font-bold mb-5">Login</h1>
-                <form onSubmit={handleSubmit}>
-                    <div>
-                        <label htmlFor='username' className='mr-2'>Username :</label>
-                        <input type="text" className="rounded-xl box-border h-11 w-150 pl-2 mt-2" id="username" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
-                    </div>
-                    <div>
-                        <label htmlFor='password' className='mr-2'>Password :</label>
-                        <input type="text" className="rounded-xl box-border h-11 w-150 pl-2 mt-2" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                    </div>
-                    <button type="submit" className="h-12 px-6 mt-3 text-lg bg-black text-white rounded-lg hover:bg-sky-700 font-bold" onClick={login}>Login</button>
-                </form>
-
-                <p className="text-2xl text-black-600 font-bold mt-6">{loginMessage}</p>
-
-                <Link to="/register" className='font-bold'>Not Registered?</Link>
-            </div>
-
-
-
-            {/* {console.log(loginStatus)}
-            {loginStatus && (
-                <button onClick={userAuthenticated}
-                    className="h-12 px-6 mt-3 text-lg bg-sky-500 rounded-lg hover:bg-sky-700">
-                    Check If Auth
-                </button>)} */}
-
+  <div class="flex flex-col bg-[#f2eedb] items-center justify-center px-6 py-20 mx-auto md:h-screen lg:py-0">
+      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                  Sign in to your account
+              </h1>
+              <form class="space-y-4 md:space-y-6" action="#">
+                  <div>
+                      <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
+                      <input type="text" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-50" required="" onChange={(e)=>{
+                        setUsername(e.target.value)
+                      }}/>
+                  </div>
+                  <div>
+                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                      <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" onChange={(e)=>{
+                        setPassword(e.target.value)
+                      }}/>
+                  </div>
+                  <div class="flex items-center justify-between">
+                      <div class="flex items-start">
+                          <div class="flex items-center h-5">
+                            <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required=""/>
+                          </div>
+                          <div class="ml-3 text-sm">
+                            <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
+                          </div>
+                      </div>
+                      <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                  </div>
+                  <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" onSubmit={(event) => {
+                    event.preventDefault()
+                  }}>Sign in</button>
+                  <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                      Don’t have an account yet? <a href="./register" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                  </p>
+              </form>
+          </div>
+      </div>
+  </div>
         </>
     )
 }
